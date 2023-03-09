cce6456e7cbd272061a94c2c65cb4934170c3120
viza <avizaanandya12@gmail.com>
2023-03-09 19:35:44 +0700

    responsive modified

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -2,10 +2,27 @@ import { Component } from "react";
 import React from "react";
 import './stylesheet.css';
 
-class Left extends React.Component{
+class Left extends Component{
     render(){
         return(
-            <div className="content">
+            <div>
+                <div className="contentLeft">
+                    <div className="contentDesc">
+                        <div className="wrapper">
+                            <h1>{this.props.title}</h1>
+                            <p>{this.props.desc}</p>
+                            <a href={this.props.link}>LEARN MORE</a>
+                        </div>
+                    </div>
+                    <div className="contentImg">
+                        <img src={this.props.image} alt="" />
+                    </div>
+                </div>
+
+                <div className="contentSwitch">
+                 <div className="contentImg">
+                    <img src={this.props.image} alt="" />
+                </div>
                 <div className="contentDesc">
                     <div className="wrapper">
                         <h1>{this.props.title}</h1>
@@ -13,10 +30,9 @@ class Left extends React.Component{
                         <a href={this.props.link}>LEARN MORE</a>
                     </div>
                 </div>
-                <div className="contentImg">
-                    <img src={this.props.image} alt="" />
-                </div>
             </div>
+            </div>
+            
         )
     }
 }
